407bdcd3383c4c11ff6ae8c245b2e29046b41545
KN <knny.nguyen@gmail.com>
2019-03-13 17:49:43 -0400

    client id bug

--- config/prod.js
+++ config/prod.js
@@ -1,5 +1,5 @@
 // production keys - grabbed from environment variables set in HEROKU
-module.export = {
+module.exports = {
   googleClientID: 'process.env.GOOGLE_CLIENT_ID',
   googleClientSecret: 'process.env.GOOGLE_CLIENT_SECRET',
   cookieKey: 'process.env.COOKIE_KEY'
