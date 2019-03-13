ac90b3328935e721cb32a48fb3b2729dc64acc12
KN <knny.nguyen@gmail.com>
2019-03-13 18:04:47 -0400

    CONFIG VARS

--- config/prod.js
+++ config/prod.js
@@ -1,6 +1,6 @@
 // production keys - grabbed from environment variables set in HEROKU
 module.exports = {
-  googleClientID: 'process.env.GOOGLE_CLIENT_ID',
-  googleClientSecret: 'process.env.GOOGLE_CLIENT_SECRET',
-  cookieKey: 'process.env.COOKIE_KEY'
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  cookieKey: process.env.COOKIE_KEY
 };
