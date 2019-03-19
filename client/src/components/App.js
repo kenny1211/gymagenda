53bc19b0b8f9069df1dbe28bd7840680f1ae4fe9
KN <knny.nguyen@gmail.com>
2019-03-19 11:59:00 -0400

    Landing Edits

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -9,9 +9,9 @@ import * as actions from '../actions';
 
 // Dummy components for now -- will be replaced w actual components
 import Header from './Header';
+import Landing from './Landing';
 const Dashboard = () => <h2>Dashboard</h2>;
 const WorkoutsNew = () => <h2>WorkoutsNew</h2>;
-const Landing = () => <h2>Landing</h2>;
 
 class App extends Component {
   componentDidMount() {
---
+++ client/src/components/Landing.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Landing = () => {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h1>Gymagenda</h1>
+      Make some workouts, do them.
+    </div>
+  );
+};
+
+export default Landing;
