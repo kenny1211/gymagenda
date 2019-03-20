1985a7aaa2aaa601981c49e01c2e570e4885b0e1
KN <knny.nguyen@gmail.com>
2019-03-19 23:16:42 -0400

    WorkoutsNew HandleSubmit

--- client/src/components/workouts/WorkoutsNew.js
+++ client/src/components/workouts/WorkoutsNew.js
@@ -1,9 +1,46 @@
 import React, { Component } from 'react';
-import WorkoutsForm from './WorkoutsForm';
+import { Form, FormGroup, Label, Input, Button } from 'reactstrap';
 
 class WorkoutsNew extends Component {
+  state = {
+    program: '',
+    workouts: []
+  };
+
+  handleChange = event => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  };
+
+  handleFormSubmit = event => {
+    event.preventDefault();
+    console.log(this.state);
+
+    // prep object to be sent to API
+
+    // make post request
+  };
+
   render() {
-    return <WorkoutsForm />;
+    return (
+      <div style={{ textAlign: 'center' }} className="container">
+        <h1>Create Workout Program</h1>
+        <Form onSubmit={this.handleFormSubmit}>
+          <FormGroup>
+            <Label>Program Name</Label>
+            <Input
+              type="text"
+              name="program"
+              onChange={this.handleChange}
+              value={this.state.program}
+            />
+          </FormGroup>
+          <Button type="submit" onClick={this.handleFormSubmit}>
+            Submit
+          </Button>
+        </Form>
+      </div>
+    );
   }
 }
 
